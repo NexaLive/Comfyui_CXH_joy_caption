5363bd2eaf050686c5f2535248a98a353fbbd7e2
unaG <veraguo11@163.com>
2025-03-09 23:04:09 +0800

    新增一个节点Joy_caption_alpha_batches

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .Joy_caption_node import Joy_caption
 from .miniCPMv2_6_prompt_generator import CXH_HG_Model_Load,CXH_Min2_6_prompt_Run
 from .florence_nodes import CXH_DownloadAndLoadFlorence2Model,CXH_Florence2Run
 from .miniCpMV3_4_chat import  CXH_MinCP3_4B_Load,CXH_MinCP3_4B_Chat
-from .Joy_caption_alpha import Joy_caption_alpha_load,Joy_caption_alpha_run,Joy_caption_alpha_prompt,Joy_caption_alpha_batch,Joy_caption_alpha_batch_Dirs
+from .Joy_caption_alpha import Joy_caption_alpha_load,Joy_caption_alpha_run,Joy_caption_alpha_prompt,Joy_caption_alpha_batch,Joy_caption_alpha_batches, Joy_caption_alpha_batch_Dirs
 from .CXH_Min2_6_classifiy import CXH_Min2_6_classifiy
 from .ic_lora_batch import CXH_IC_Lora_Florence2Run,CXH_Ic_lora_Joy_batch,CXH_IC_lora_reversal
 from .smolvlm import CXH_SmolVlm_Load,CXH_SmolVlm_Run
@@ -22,6 +22,7 @@ NODE_CLASS_MAPPINGS = {
     "Joy_caption_alpha_run":Joy_caption_alpha_run,
     "Joy_caption_alpha_prompt":Joy_caption_alpha_prompt,
     "Joy_caption_alpha_batch":Joy_caption_alpha_batch,
+    "Joy_caption_alpha_batches":Joy_caption_alpha_batches,
     "CXH_Min2_6_classifiy":CXH_Min2_6_classifiy,
     "CXH_Ic_lora_Joy_batch":CXH_Ic_lora_Joy_batch,
     "CXH_IC_Lora_Florence2Run":CXH_IC_Lora_Florence2Run,
@@ -44,6 +45,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Joy_caption_alpha_run":"Joy_caption_alpha_run",
     "Joy_caption_alpha_prompt":"Joy_caption_alpha_prompt",
     "Joy_caption_alpha_batch":"Joy_caption_alpha_batch",
+    "Joy_caption_alpha_batches":"Joy_caption_alpha_batches",
     "CXH_Min2_6_classifiy":"CXH_Min2_6_classifiy",
     "CXH_Ic_lora_Joy_batch":"CXH_Ic_lora_Joy_batch",
     "CXH_IC_Lora_Florence2Run":"CXH_IC_Lora_Florence2Run",
